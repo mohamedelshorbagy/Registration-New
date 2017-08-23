f7bd9f7d159dc6cb53a19a8414a0efb46c2b8c89
mohamedelshorbagy <mohamedelshorbagy96@gmail.com>
2017-08-24 00:09:59 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,63 @@
+
+
+
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="utf-8" />
+        <link rel="shortcut icon" href="images/EED Logo 2017.png" />
+        <title>Terms & Condition</title>
+
+        <link rel="stylesheet" type="text/css" href="https://bootswatch.com/readable/bootstrap.min.css">
+        <style>
+
+
+            body {
+                    background:#f1efd6;
+            }
+         .visit-img {
+            margin: 0 auto;
+
+            }
+            
+
+
+        </style>
+    </head>
+    <body>
+
+                 <a href="http://eed.eg">
+        <img src="images/LOGOS PNG.png" class="img-responsive" alt="EED Logo" />
+                </a>
+
+
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-8 col-md-offset-2">
+                    <img src="images/visitor.png" alt="Visitor Registration" class="img-responsive visit-img"/>
+        
+                        <div class="alert alert-danger text-center">Registration is CLOSED</div>
+                    
+                    
+                    
+                    </div>
+                
+                </div>
+            
+            
+            
+            
+            </div>
+
+
+
+
+
+
+
+
+
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+    </body>
+</html>
